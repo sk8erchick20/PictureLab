b6061a361024741bc27791f1d10740cddaa41107
Karishma <tikkysood@yahoo.com>
2015-02-25 08:14:35 -0700

    Update IntArrayWorker.java

--- pixLab/classes/IntArrayWorker.java
+++ pixLab/classes/IntArrayWorker.java
@@ -98,5 +98,34 @@ public class IntArrayWorker
       }
     }
   }
- 
+  public int getCount(int n)
+  {
+    int count=0;
+    for(int i=0; i<matrix.length; i++)
+    {
+      for(int j=0;j<matrix[i].length;j++)
+      {
+        if(matrix[i][j]==n)
+        {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+ public int getLargest()
+ {
+   int n=0;
+   for(int i=0; i<matrix.length; i++)
+    {
+      for(int j=0;j<matrix[i].length;j++)
+      {
+        if(matrix[i][j]>=n)
+        {
+          n=matrix[i][j];
+        }
+      }
+   }
+   return n;
+ }
 }
